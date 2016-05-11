82b23f3324f4f34465a04b7d40c5812fd412be38
Sagar Makwana <smakwana@usc.edu>
2016-05-11 14:49:40 -0700

    Exception Handling done

--- src/edu/usc/cssl/tacit/crawlers/typepad/TypePadCrawler.java
+++ src/edu/usc/cssl/tacit/crawlers/typepad/TypePadCrawler.java
@@ -3,9 +3,12 @@ package edu.usc.cssl.tacit.crawlers.typepad;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,52 +16,82 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
 
 
 public class TypePadCrawler {
 	public static String OUTPUT_PATH = "/Users/CSSLadmin/Desktop/TypePadOutput";
-
+	
 	public static void main(String[] args) throws Exception{
 		
 		TypePadCrawler typePadCrawler = new TypePadCrawler();
-		typePadCrawler.getQueryResults("star wars");
+		typePadCrawler.getQueryResults("star wars",1);
 	
 	}
 	
-	//TODO: Improve Http connection code
-	private String getHTTPResponse(String url) throws Exception {
-
+	/**
+	 * This method returns the JSON HTTP response string for the input string URL 
+	 * @param url Connection URL
+	 * @return JSON response string if the connection was successful and response was received else it returns an empty string.
+	 */
+	private String getHTTPResponse(String url) {
 		
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-		con.setRequestMethod("GET");
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		BufferedReader br = null;
+		HttpURLConnection con = null;
 		String inputLine;
 		StringBuffer response = new StringBuffer();
+		try{
+			URL obj = new URL(url);
+			con = (HttpURLConnection) obj.openConnection();
 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
+			con.setRequestMethod("GET");
+			con.setConnectTimeout(15000);
 
-		//System.out.println(response.toString());
+			int responseCode = con.getResponseCode();
+			
+			if(responseCode == HttpURLConnection.HTTP_OK){
+				System.out.println("\nSending 'GET' request to URL : " + url);
+				System.out.println("Response Code : " + responseCode);
+
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				while ((inputLine = br.readLine()) != null) {
+					response.append(inputLine);
+				}
+				br.close();
 		
-		return response.toString();
+				return response.toString();
+			}else{
+				System.out.println("Internal Connection Error.");
+				return "";
+			}
+			
+		}catch(SocketTimeoutException e){
+			System.out.println("Connection is taking too long.There is something wrong with the server.");
+			System.out.println(e.getMessage());
+			return "";
+		}catch(UnknownHostException e){
+			System.out.println("There seems to be no internet connection.");
+			System.out.println(e.getMessage());
+			return "";
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			return "";
+		}finally{
+			if (con != null){
+				con.disconnect();
+			}
+		}
+		
 
 	}
 	
 	/**
 	 * This method builds a query string from the raw query string.The returned query can be directly added to the URL to interact with the API and retrieve results.
 	 * @param rawQuery (eg. star wars)
+	 * @param sortParam 0: published_time_relevance, 1: relevance, 2: published_time_asc, 3: published_time_desc, -1: no sort 
 	 * @return buildQuery (eg. q=star%20wars)
 	 */
-	private String getQuerySubURL(String rawQuery){
+	private String buildQuery(String rawQuery, int sortParam){
 		rawQuery = rawQuery.trim();
 		String queryKeywords [] = rawQuery.split(" ");
 		
@@ -72,7 +105,19 @@ public class TypePadCrawler {
 
 		buildQuery.delete(buildQuery.length()-3, buildQuery.length());
 		finalBuildQuery = buildQuery.toString();
+		
+		
+		//TODO: Temporary lines of code. Can be removed
+		finalBuildQuery = "\""+ finalBuildQuery + "\"";
+		finalBuildQuery = "title:"+finalBuildQuery;
+		
+		
+		
 		finalBuildQuery = WebConstants.QUERY+finalBuildQuery;
+		
+		if (sortParam >= 0){
+			finalBuildQuery = finalBuildQuery + WebConstants.PARAM_SEPARATOR + WebConstants.SORT + WebConstants.SORT_PARAMS[sortParam];
+		}
 		
 		return finalBuildQuery;
 		
@@ -81,19 +126,21 @@ public class TypePadCrawler {
 	/**
 	 * This method outputs the query result blogs into files. 
 	 * @param rawQuery (eg. star wars)
+	 * @param sortParam 0: published_time_relevance, 1: relevance, 2: published_time_asc, 3: published_time_desc, -1: no sort
 	 * @throws Exception
 	 */
-	public void getQueryResults(String rawQuery)throws Exception{
+	public void getQueryResults(String rawQuery, int sortParam)throws Exception{
 
 		//Initial Query Results
-		String url = WebConstants.BASE_URL+WebConstants.ASSETS+WebConstants.PARAM_SEPARATOR;
+		String url = WebConstants.BASE_URL+WebConstants.ASSETS+WebConstants.QUERY_SEPARATOR;
 		//Adding the query string
-		url = url + getQuerySubURL("star wars");
+		url = url + buildQuery("star wars",sortParam);
 		
 		//Default Variable Declaration 
 		JSONObject resultJSONObject = null; 
 		JSONArray retrievedEntriesArray = null;
 		String moreResultsToken = "";
+		String httpResponse = "";
 		FileWriter fw = null;
 		int blogCount = 1;
 		
@@ -102,32 +149,37 @@ public class TypePadCrawler {
 		String commonFileName = new SimpleDateFormat("yyyyMMddhhmm'.txt'").format(currentDate);
 		
 		do{
+			
+			httpResponse = getHTTPResponse(url);
+			if (!httpResponse.equals("")){
+				resultJSONObject = new JSONObject(getHTTPResponse(url));
+				retrievedEntriesArray = resultJSONObject.getJSONArray("entries");
+				try{
+					moreResultsToken = resultJSONObject.getString("moreResultsToken");
+				}catch(JSONException e){
+					moreResultsToken = null;
+				}
+				
+				
+				JSONObject retrievedEntryObject = null;
+				String retrievedEntryContent = "";
+				String finalEntryContent = "";
+				
+				for (int i= 0; i<retrievedEntriesArray.length();i++ ){
+					fw = new FileWriter(new File(OUTPUT_PATH + File.separator + "blog_"+blogCount+"_"+commonFileName));
+					retrievedEntryObject = (JSONObject)retrievedEntriesArray.get(i);
+					retrievedEntryContent = retrievedEntryObject.getString("content");
+					finalEntryContent = Jsoup.parse(retrievedEntryContent).text();
+					fw.write(finalEntryContent);
+					fw.close();
+					blogCount++;
+				}
+				
+				url = WebConstants.BASE_URL+WebConstants.ASSETS+WebConstants.QUERY_SEPARATOR+WebConstants.START_TOKEN + moreResultsToken;
+			}else{
+				break;
+			}
 
-			resultJSONObject = new JSONObject(getHTTPResponse(url));
-			retrievedEntriesArray = resultJSONObject.getJSONArray("entries");
-			try{
-				moreResultsToken = resultJSONObject.getString("moreResultsToken");
-			}catch(JSONException e){
-				moreResultsToken = null;
-			}
-			
-			
-			JSONObject retrievedEntryObject = null;
-			String retrievedEntryContent = "";
-			String finalEntryContent = "";
-			
-			for (int i= 0; i<retrievedEntriesArray.length();i++ ){
-				fw = new FileWriter(new File(OUTPUT_PATH + File.separator + "blog_"+blogCount+"_"+commonFileName));
-				retrievedEntryObject = (JSONObject)retrievedEntriesArray.get(i);
-				retrievedEntryContent = retrievedEntryObject.getString("content");
-				finalEntryContent = Jsoup.parse(retrievedEntryContent).text();
-				fw.write(finalEntryContent);
-				fw.close();
-				blogCount++;
-			}
-			
-			url = WebConstants.BASE_URL+WebConstants.ASSETS+WebConstants.PARAM_SEPARATOR+WebConstants.START_TOKEN + moreResultsToken;
-			
 		}while(moreResultsToken != null);
 	}
 
--- src/edu/usc/cssl/tacit/crawlers/typepad/WebConstants.java
+++ src/edu/usc/cssl/tacit/crawlers/typepad/WebConstants.java
@@ -4,17 +4,31 @@ public class WebConstants {
 	
 	public static final String BASE_URL = "http://api.typepad.com";
 	
-	public static final String BLOGS = "/blogs";
-	
-	//Assets URl Constants
+	//Assets URL Constants
 	public static final String ASSETS = "/assets.json";
 	
 	public static final String START_TOKEN = "start-token=";
 	
-	public static final String PARAM_SEPARATOR = "?";
-	
 	public static final String QUERY = "q=";
 	
-	public static final String MAX_RESULTS = "&max-results=";
+	public static final String MAX_RESULTS = "max-results=";
+	
+	public static final String SORT = "sort=";
+	
+	public static final String PARAM_SEPARATOR = "&";
+	
+	public static final String QUERY_SEPARATOR = "?";
+	
+	
+	// Sort Params
+	public static final String PUBLISHED_TIME_RELEVANCE = "published_time_relevance";
+	
+	public static final String RELEVANCE = "relevance";
+	
+	public static final String PUBLISHED_TIME_ASC = "published_time_asc";
+	
+	public static final String PUBLISHED_TIME_DESC = "published_time_desc";
+	
+	public static final String[] SORT_PARAMS = {PUBLISHED_TIME_RELEVANCE, RELEVANCE, PUBLISHED_TIME_ASC, PUBLISHED_TIME_DESC};
 
 }
